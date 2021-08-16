67e605e9ddb115cc39729df27232bbc669e0f542
Shades-2 <sonnyhartney@gmail.com>
2021-08-16 15:08:16 +0100

    dotenv in progress

--- src/app.py
+++ src/app.py
@@ -1,16 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
+from dotenv import dotenv_values
+
+config = dotenv_values('.env')
+
+user_name = environ['USER']
+password = environ['PASSWORD']
+secret_key = environ['SECRET_KEY']
 
 
 def create_app(test=False):
     app = Flask(__name__)
-    if not test:
-        app.config['SECRET_KEY'] = 'dev'
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:dev@localhost/cleaner'
+    app.config['SECRET_KEY'] = f'{secret_key}'
+    if test:
+        app.config['SQLALCHEMY_DATABASE_URI'] = (
+            f'postgresql+psycopg2://{user_name}:{password}@localhost/unittest-cleaner'
+        )
     else:
-        app.config['SECRET_KEY'] = 'test'
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:dev@localhost/unittest-cleaner'
-
+        app.config['SQLALCHEMY_DATABASE_URI'] = (
+            f'postgresql+psycopg2://{user_name}:{password}@localhost/cleaner'
+        )
     return app
 
 
