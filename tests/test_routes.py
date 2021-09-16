7438664f179d36d49540da7331995228daa76591
Shades-2 <sonnyhartney@gmail.com>
2021-09-16 16:42:44 +0100

    in progress

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -11,23 +11,27 @@ class TestAPI(BaseTest):
             'email': 'bar@gmail.com',
             'services': 'x'
         }
+        self.cleaner = Cleaner(**self.test_user)
+        db.session.add(self.cleaner)
+        db.session.commit()
 
     def test_add_cleaner(self):
-        response = self.client.post('/cleaner', json=self.test_user)
+        response = self.client.post('/cleaner', json={
+            'username': 'harry',
+            'email': 'harry@gmail.com',
+            'services': 'everything'
+        })
 
         # Check response is correct
         self.assertEqual(response.status_code, 201)
 
         # Retrieve newly created cleaner object from database and check it's correct
-        cleaner = db.session.query(Cleaner).filter_by(username='foo').first()
-        self.assertEqual(cleaner.username, self.test_user['username'])
-        self.assertEqual(cleaner.email, self.test_user['email'])
-        self.assertEqual(cleaner.services, self.test_user['services'])
+        cleaner = db.session.query(Cleaner).filter_by(username='harry').first()
+        self.assertEqual(cleaner.username, 'harry')
+        self.assertEqual(cleaner.email, 'harry@gmail.com')
+        self.assertEqual(cleaner.services, 'everything')
 
     def test_get_cleaner(self):
-        cleaner = Cleaner(**self.test_user)
-        db.session.add(cleaner)
-        db.session.commit()
         response = self.client.get('/cleaner', query_string={'username': self.test_user['username']})
         self.assertEqual(response.status_code, 200)
         return_details = response.json
@@ -36,8 +40,6 @@ class TestAPI(BaseTest):
     # TODO test get cleaner no username provided
 
     def test_add_cleaner_with_non_unique_username(self):
-        db.session.add(self.test_user)
-        db.session.commit()
         response = self.client.post(
             '/cleaner',
             json=self.test_user)
@@ -45,9 +47,8 @@ class TestAPI(BaseTest):
         self.assertEqual(response.json['error'], 'non unique username')
 
     def test_add_cleaner_with_non_unique_email(self):
-        cleaner_details_with_non_unique_email = {'username': 'johnny', 'bar@gmail.com': 'c', 'services': 'klj'}
-        db.session.add(self.test_user)
-        db.session.commit()
+        cleaner_details_with_non_unique_email = {'username': 'johnny', 'email': 'bar@gmail.com', 'services': 'klj'}
+        cleaner = Cleaner(**cleaner_details_with_non_unique_email)
         response = self.client.post('/cleaner', json=cleaner_details_with_non_unique_email)
         self.assertEqual(response.status_code, 400)
 
